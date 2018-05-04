d97773a109a3f3c70e3323148e465633690c9ff6
Edwyn Batista <edwynluis@yahoo.com.br>
2018-05-04 14:30:14 +0200

    test example

--- test/Inbox.text.js
+++ test/Inbox.text.js
@@ -2,3 +2,35 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+
+
+/*
+
+// TEST EXAMPLE
+
+class Car{
+	park(){
+		return 'stopped';
+	}
+	drive(){
+		return 'vroom';
+	}
+}
+
+let car;
+
+beforeEach( () => {
+	car = new Car();
+} );
+
+describe('Block 1', () => {
+	it('Test 1 : park', () => {
+		assert.equal( car.park(), 'stopped' );
+	});
+	it('Test 2 : drive', () => {
+		assert.equal( car.drive(), 'vroom' );
+	});
+});
+
+*/
